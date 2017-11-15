64bc373cc683822b6d2b924e745e05e79e20c3e0
edniemand <edniemand@users.noreply.github.com>
2017-11-15 10:54:37 -0700

    Add files via upload

--- sale_order.py
+++ sale_order.py
@@ -12,34 +12,34 @@ class sale_order(models.Model):
 
 	# @api.multi
 	# def action_confirm(self):
-	# 	if self.env['sale.order'].search([('partner_id', '=', self.partner_id.id),\
-	# 		('client_order_ref', '=', self.client_order_ref),\
-	# 		('state', '!=', self.state)]):
-	# 		raise ValidationError(_('La referencia ya se ha usado para este cliente.'))
-	# 	#else:
-	# 		return super(sale_order,self).action_confirm()	
+	#   if self.env['sale.order'].search([('partner_id', '=', self.partner_id.id),\
+	#       ('client_order_ref', '=', self.client_order_ref),\
+	#       ('state', '!=', self.state)]):
+	#       raise ValidationError(_('La referencia ya se ha usado para este cliente.'))
+	#   #else:
+	#       return super(sale_order,self).action_confirm()  
 
 
 	@api.onchange('client_order_ref')
-    def _onchange_client_order_ref(self):
-        if self.env['sale.order'].search([('partner_id', '=', self.partner_id.id),\
-	 		('client_order_ref', '=', self.client_order_ref),\
-	 		('state', '!=', self.state)]):
-                    warning_mess = {
-                        'title': _('ValidationError'),
-                        'message' : _('La referencia ya se ha usado para este cliente.')
-                    }
-                    return {'warning': warning_mess}
-        return {}
+	def _onchange_client_order_ref(self):
+		if self.env['sale.order'].search([('partner_id', '=', self.partner_id.id),\
+			('client_order_ref', '=', self.client_order_ref),\
+			('state', '!=', self.state)]):
+					warning_mess = {
+						'title': _('ValidationError'),
+						'message' : _('La referencia ya se ha usado para este cliente.')
+					}
+					return {'warning': warning_mess}
+		return {}
 
 
 		# return {
-		# 		'type': 'ir.actions.client',
-		# 		'tag': 'action_warn'
-		# 		'name': _('Aviso'),
-		# 		'context': context,
-		# 		'params' : {
-		# 			'title' : _('Aviso'),
-		# 			'text' : _('Referencia interna del cliente ya ha sido utilizada previamente.'),
-		# 			'sticky' : True
-		# 		}}
+		#       'type': 'ir.actions.client',
+		#       'tag': 'action_warn'
+		#       'name': _('Aviso'),
+		#       'context': context,
+		#       'params' : {
+		#           'title' : _('Aviso'),
+		#           'text' : _('Referencia interna del cliente ya ha sido utilizada previamente.'),
+		#           'sticky' : True
+		#       }}
