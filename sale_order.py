3d8acf4538183f610ee83ba66f5bf0382b8786ba
edniemand <edniemand@users.noreply.github.com>
2017-11-15 10:04:22 -0700

    Add files via upload

--- sale_order.py
+++ sale_order.py
@@ -12,16 +12,14 @@ class sale_order(models.Model):
 
 	@api.multi
 	def action_confirm(self):
-		#result = super(sale_order,self).action_confirm()
+		result = super(sale_order,self).action_confirm()
 		if self.env['sale.order'].search([('partner_id', '=', self.partner_id.id),\
 			('client_order_ref', '=', self.client_order_ref),\
 			('state', '!=', self.state)]):
 			raise ValidationError(_('La referencia ya se ha usado para este cliente.'))
 		#else:
-		return super(sale_order,self).action_confirm()
-		
-
-		#eturn result
+			#return super(sale_order,self).action_confirm()	
+		return result
 
 		# return {
 		# 		'type': 'ir.actions.client',
