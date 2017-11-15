f8d800b8774d71a356750d05208f9385f03aab20
edniemand <edniemand@users.noreply.github.com>
2017-11-15 09:57:14 -0700

    Add files via upload

--- sale_order.py
+++ sale_order.py
@@ -11,10 +11,25 @@ class sale_order(models.Model):
 	client_order_ref = fields.Char(string='Customer Reference', copy=False, required=True)
 
 	@api.multi
-	def action_confirm(self):	
+	def action_confirm(self):
+		#result = super(sale_order,self).action_confirm()
 		if self.env['sale.order'].search([('partner_id', '=', self.partner_id.id),\
 			('client_order_ref', '=', self.client_order_ref),\
 			('state', '!=', self.state)]):
 			raise ValidationError(_('La referencia ya se ha usado para este cliente.'))
-		else:
-			return super(sale_order,self).action_confirm()
+		#else:
+		return super(sale_order,self).action_confirm()
+		
+
+		#eturn result
+
+		# return {
+		# 		'type': 'ir.actions.client',
+		# 		'tag': 'action_warn'
+		# 		'name': _('Aviso'),
+		# 		'context': context,
+		# 		'params' : {
+		# 			'title' : _('Aviso'),
+		# 			'text' : _('Referencia interna del cliente ya ha sido utilizada previamente.'),
+		# 			'sticky' : True
+		# 		}}
