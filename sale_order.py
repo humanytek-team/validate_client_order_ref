a3df477f4dcbd96adf624b45b24af0ddb858616e
edniemand <edniemand@users.noreply.github.com>
2017-11-15 10:53:27 -0700

    Add files via upload

--- sale_order.py
+++ sale_order.py
@@ -10,16 +10,28 @@ class sale_order(models.Model):
 
 	client_order_ref = fields.Char(string='Customer Reference', copy=False, required=True)
 
-	@api.multi
-	def action_confirm(self):
-		result = super(sale_order,self).action_confirm()
-		if self.env['sale.order'].search([('partner_id', '=', self.partner_id.id),\
-			('client_order_ref', '=', self.client_order_ref),\
-			('state', '!=', self.state)]):
-			raise ValidationError(_('La referencia ya se ha usado para este cliente.'))
-		#else:
-			#return super(sale_order,self).action_confirm()	
-		return result
+	# @api.multi
+	# def action_confirm(self):
+	# 	if self.env['sale.order'].search([('partner_id', '=', self.partner_id.id),\
+	# 		('client_order_ref', '=', self.client_order_ref),\
+	# 		('state', '!=', self.state)]):
+	# 		raise ValidationError(_('La referencia ya se ha usado para este cliente.'))
+	# 	#else:
+	# 		return super(sale_order,self).action_confirm()	
+
+
+	@api.onchange('client_order_ref')
+    def _onchange_client_order_ref(self):
+        if self.env['sale.order'].search([('partner_id', '=', self.partner_id.id),\
+	 		('client_order_ref', '=', self.client_order_ref),\
+	 		('state', '!=', self.state)]):
+                    warning_mess = {
+                        'title': _('ValidationError'),
+                        'message' : _('La referencia ya se ha usado para este cliente.')
+                    }
+                    return {'warning': warning_mess}
+        return {}
+
 
 		# return {
 		# 		'type': 'ir.actions.client',
